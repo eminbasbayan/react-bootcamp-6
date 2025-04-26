c9506d3262062f4daff14e558ae8e5569f2f8076
Emin Basbayan <emin.basbayan@icloud.com>
2025-04-26 22:35:03 +0300

    react componentlerinde javascript değişkenleri tanımlama

--- src/ProductItem.jsx
+++ src/ProductItem.jsx
@@ -1,17 +1,19 @@
 import "./ProductItem.css";
 
 function ProductItem() {
+  const image =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREaXGeoatJyRmP9Aa-q4zaqgcUQim3bQHvjg&usqp=CAU";
+  const title = "Soda";
+  const price = 10;
+
   return (
     <div className="product-item">
       <div className="product-image">
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREaXGeoatJyRmP9Aa-q4zaqgcUQim3bQHvjg&usqp=CAU"
-          alt="Soda"
-        />
+        <img src={image} alt="Soda" />
       </div>
       <div className="product-info">
-        <b className="product-title">Soda</b>
-        <span className="product-price">4₺</span>
+        <b className="product-title">{title}</b>
+        <span className="product-price">{price}₺</span>
       </div>
     </div>
   );
