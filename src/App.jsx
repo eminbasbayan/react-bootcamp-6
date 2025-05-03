ea26cf2fb43af2de42299fedd53939aa38cf0a3a
Emin Basbayan <emin.basbayan@icloud.com>
2025-05-03 21:11:52 +0300

    react fragment ve empty tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
+import React from "react";
 import Button from "./components/UI/Button";
 import Products from "./components/Products/Products";
 
 function App() {
   return (
-    <div>
-      <Button color="primary" size="lg">Tıkla</Button>
+    <React.Fragment>
+      <Button color="primary" size="lg">
+        Tıkla
+      </Button>
       <Button color="danger" size="lg">
         <b>Ürünü Sil</b>
       </Button>
 
       <Products />
-    </div>
+    </React.Fragment>
   );
 }
 
