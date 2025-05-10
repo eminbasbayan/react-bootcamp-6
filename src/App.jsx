fc59a2cb6f8a713b9f8dbb04bf5f75aeabe987c5
Emin Basbayan <emin.basbayan@icloud.com>
2025-05-10 23:23:56 +0300

    loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import Products from "./components/Products/Products";
-import Users from "./components/Users";
 
 function App() {
   return (
     <div className="app container mx-auto">
-      <Users />
       <Products />
     </div>
   );
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,19 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddProduct from "./AddProduct";
 import ProductItem from "./ProductItem";
 import Modal from "../UI/Modal";
 
-import { productsData } from "../../data/productsData";
 import "./Products.css";
 
 function Products() {
-  const [products, setProducts] = useState(productsData);
+  const [products, setProducts] = useState([]);
   const [isShowModal, setIsShowModal] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   function handleDeleteItem(productId) {
     const filteredProducts = products.filter((item) => item.id !== productId);
     setProducts(filteredProducts);
   }
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const res = await fetch("https://fakestoreapi.com/products");
+        const data = await res.json();
+
+        setProducts(data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchProducts();
+  }, []);
 
   return (
     <div className="products">
@@ -26,6 +43,7 @@ function Products() {
           desc="Bu bir modal içerik örneğidir."
         />
       )}
+      {isLoading && <p>Ürünler yükleniyor...</p>}
       <div className="products-wrapper">
         {products.map((product) => (
           <ProductItem
