61aa3bfd24d4d99737c2839d201be475a2c42e8b
Emin Basbayan <emin.basbayan@icloud.com>
2025-05-23 23:08:48 +0300

    react redux kurulumu - provider component

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import CartProvider from './context/CartProvider';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import CartProvider from "./context/CartProvider";
+import ThemeProvider from "./context/ThemeProvider";
 
-import './index.css';
-import ThemeProvider from './context/ThemeProvider';
+import store from "./redux/store";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <ThemeProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </ThemeProvider>
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <ThemeProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </Provider>
 );
