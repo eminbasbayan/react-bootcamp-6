85f729d58822def6f7149412f3ede30e7deff098
Emin Basbayan <emin.basbayan@icloud.com>
2025-05-24 22:44:25 +0300

    feat: add ContactPage component and route to App

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import { ThemeContext } from "./context/ThemeProvider";
 import Header from "./components/Layout/Header";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
-import Counter from "./components/Counter";
 import AboutPage from "./pages/AboutPage";
 import ProductsPage from "./pages/ProductsPage";
+import ContactPage from "./pages/ContactPage";
 
 function App() {
   const { darkMode } = useContext(ThemeContext);
@@ -28,6 +28,7 @@ function App() {
           <Route path="/products" element={<ProductsPage />} />
           <Route path="/cart" element={<CartPage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </div>
     </div>
