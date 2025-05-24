a5bbdc1153bafa1fd687e9d2ad356ba122c593f2
Emin Basbayan <emin.basbayan@icloud.com>
2025-05-24 22:40:31 +0300

    feat: add About and Products pages, enhance HomePage layout and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useContext } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import { ThemeContext } from './context/ThemeProvider';
-import Header from './components/Layout/Header';
-import HomePage from './pages/HomePage';
-import CartPage from './pages/CartPage';
-import Counter from './components/Counter';
+import { useContext } from "react";
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { ThemeContext } from "./context/ThemeProvider";
+import Header from "./components/Layout/Header";
+import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
+import Counter from "./components/Counter";
+import AboutPage from "./pages/AboutPage";
+import ProductsPage from "./pages/ProductsPage";
 
 function App() {
   const { darkMode } = useContext(ThemeContext);
@@ -14,17 +16,18 @@ function App() {
     <div
       className="app container mx-auto"
       style={{
-        backgroundColor: `${darkMode ? 'black' : 'white'}`,
-        color: `${darkMode ? 'white' : 'black'}`,
+        backgroundColor: `${darkMode ? "black" : "white"}`,
+        color: `${darkMode ? "white" : "black"}`,
       }}
     >
       <ToastContainer />
       <Header />
       <div className="pt-4">
-        <Counter />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductsPage />} />
           <Route path="/cart" element={<CartPage />} />
+          <Route path="/about" element={<AboutPage />} />
         </Routes>
       </div>
     </div>
